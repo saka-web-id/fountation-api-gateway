d05151cf8ae033cedfeb904da74c9a9d5a94cd38
sakoen <sakoen_not_death@yahoo.com>
2025-11-27 00:29:47 +0700

    Spring OAuth2 and Gateway update

---
+++ src/main/java/id/web/saka/fountation/apigateway/Config.java
@@ -0,0 +1,33 @@
+package id.web.saka.fountation.apigateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class Config {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(auth -> auth.anyExchange().authenticated())
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder(@Value("${spring.security.oauth2.client.provider.spring.issuer-uri}") String issuerUri) {
+        return ReactiveJwtDecoders.fromIssuerLocation(issuerUri);
+    }
+
+}
