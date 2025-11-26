c5d497ae556ba3042a82f8229ea09803149e8d58
sakoen <sakoen_not_death@yahoo.com>
2025-11-26 22:14:53 +0700

    Initial commit

---
+++ src/main/java/id/web/saka/fountation/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package id.web.saka.fountation.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
